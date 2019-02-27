c458de55ca7fa488df96350bed6eccf6b3e13d03
Maksim Sinik <maksim@sinik.it>
2019-02-27 19:03:38 +0100

    Ports session to TypeScript
    Adds missing typings

---
+++ src/strategies/session.ts
@@ -0,0 +1,72 @@
+import pause from 'pause'
+import { Strategy } from 'passport-strategy'
+import { Request } from 'express'
+
+class SessionStrategy extends Strategy {
+  name: string
+  _deserializeUser: Function
+
+  constructor(options: Function | object, deserializeUser?: Function) {
+    super()
+    if (typeof options === 'function') {
+      deserializeUser = options
+      options = undefined as any
+    }
+    options = options || {}
+
+    this.name = 'session'
+    this._deserializeUser = deserializeUser!
+  }
+  /**
+   * Authenticate request based on the current session state.
+   *
+   * The session authentication strategy uses the session to restore any login
+   * state across requests.  If a login session has been established, `req.user`
+   * will be populated with the current user.
+   *
+   * This strategy is registered automatically by Passport.
+   *
+   * @param {Object} req
+   * @param {Object} options
+   * @api protected
+   */
+  authenticate(req: Request, options: { pauseStream?: boolean }) {
+    if (!req._passport) {
+      return this.error(new Error('passport.initialize() middleware not in use'))
+    }
+    options = options || {}
+
+    const self = this
+    let su
+    if (req._passport.session) {
+      su = req._passport.session.user
+    }
+
+    if (su || su === 0) {
+      // NOTE: Stream pausing is desirable in the case where later middleware is
+      //       listening for events emitted from request.  For discussion on the
+      //       matter, refer to: https://github.com/jaredhanson/passport/pull/106
+
+      const paused = options.pauseStream ? pause(req) : null
+      this._deserializeUser(su, req, function(err: Error, user: any) {
+        if (err) {
+          return self.error(err)
+        }
+        if (!user) {
+          delete req._passport.session.user
+        } else {
+          // TODO: Remove instance access
+          const property = req._passport.instance._userProperty || 'user'
+          req[property] = user
+        }
+        self.pass()
+        if (paused) {
+          paused.resume()
+        }
+      })
+    } else {
+      self.pass()
+    }
+  }
+}
+export default SessionStrategy
